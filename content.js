5748bfb1f794c64db2b25cf5b366e70078569d97
Matt <jestir1234@gmail.com>
2018-11-07 17:32:06 +0900

    add clear interval

--- content.js
+++ content.js
@@ -1,16 +1,25 @@
 window.onload = function(){
     setTimeout(function(){
         var startBtn = document.createElement("button");
-        startBtn.innerHTML = "START BUTTON";
-        startBtn.style = "background-color:red; position: absolute; height: 50px; width: 100px; color: white";
+        startBtn.innerHTML = "START SURVEY";
+        startBtn.style = "background-color:#28616d; position: absolute; left: 10px; bottom: 50%; height: 50px; width: 100px; color: white; font-weight: bold";
 
+  
         startBtn.addEventListener('click', () => {
-            setInterval(function(){
+            var startSurvey = setInterval(function(){
                 var firstInput = document.querySelector("input");
+                if (!firstInput){
+                    stopSurvey();
+                    return;
+                };
                 var nextBtn = document.querySelector(".fl-survey-new-button");
                 firstInput.click();
                 nextBtn.click();
             }, 10)
+
+            function stopSurvey() {
+                clearInterval(startSurvey);
+            }
         })
         document.querySelector('body').appendChild(startBtn);
     }, 2000)
